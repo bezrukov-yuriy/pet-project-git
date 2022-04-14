fb7fb76d2fae5418a668fb79739ee029e779942f
Yuriy Bezrukov <yuriy.bezrukov@veeam.com>
2022-04-15 01:33:04 +0300

    add material + did not according to the task of the interview

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'pet-project-git';
+
+  constructor(private titleService: Title) {}
+
+  ngOnInit(): void {
+    this.setTitle('Pet project: git search');
+  }
+
+  private setTitle(newTitle: string) {
+    this.titleService.setTitle(newTitle);
+  }
 }
--- src/app/modules/github-search/github-search.module.ts
+++ src/app/modules/github-search/github-search.module.ts
@@ -1,9 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { GithubSearchComponent } from './github-search/github-search.component';
 import { ReactiveFormsModule } from '@angular/forms';
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
+import { MatInputModule } from '@angular/material/input';
+import { MatCardModule } from '@angular/material/card';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatBadgeModule } from '@angular/material/badge';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
+import { GithubSearchComponent } from './github-search/github-search.component';
 import { RepositoryCardComponent } from './repository-card/repository-card.component';
 import { GithubSearchRoutingModule } from './github-search.routing';
+import { SharedModule } from 'src/app/shared/shared/shared.module';
 
 
 @NgModule({
@@ -18,7 +25,13 @@ import { GithubSearchRoutingModule } from './github-search.routing';
     CommonModule,
     GithubSearchRoutingModule,
     ReactiveFormsModule,
-
+    MatAutocompleteModule,
+    MatInputModule,
+    MatCardModule,
+    MatDividerModule,
+    MatProgressBarModule,
+    MatBadgeModule,
+    SharedModule,
   ]
 })
 export class GithubSearchModule { }
